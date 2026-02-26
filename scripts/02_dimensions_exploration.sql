a2eea6d832682b420941526757920cd21a52d402
Anchif <ancheart913@gmail.com>
2026-02-26 11:25:45 +0100

    Create 02_dimensions_exploration.sql

---
+++ scripts/02_dimensions_exploration.sql
@@ -0,0 +1,14 @@
+-- ================================================================
+-- Exploring Dimenson tables
+-- Identifying the unique values (or categories) in each dimension.
+-- Formula: DISTINCT [Dimension]
+-- ================================================================
+
+-- Explore all Countries our customers come from.
+SELECT DISTINCT country
+FROM gold.dim_customers;
+
+-- Explore all Categories "The major Divisions"
+SELECT DISTINCT category, subcategory, product_name
+FROM gold.dim_products
+ORDER BY 1,2,3;
