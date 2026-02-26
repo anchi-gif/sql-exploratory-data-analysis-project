de45fbd55c299e86c91131235a79c9c637d68b3a
Anchif <ancheart913@gmail.com>
2026-02-26 11:52:46 +0100

    Update 04_measures_exploration.sql

--- scripts/04_measures_exploration.sql
+++ scripts/04_measures_exploration.sql
@@ -1,7 +1,7 @@
 -- ======================================================
 -- Measures Exploration 
 -- Purpose: Calculate key metric of the business.
--- Formula: Summarize [Measure] (e.g., SUM(sales)).
+-- Formula: Aggregate [Measure] (e.g., SUM(sales)).
 -- =======================================================
 
 -- Find Total Sales.
@@ -31,3 +31,19 @@ FROM gold.dim_customers;
 -- Total number of Customers that has placed an order.
 SELECT COUNT(DISTINCT customer_key) AS total_customers
 FROM gold.fact_sales;
+
+-- ====================================================================
+-- Generate a Report that shows all key metrics of the business
+-- ====================================================================
+
+SELECT 'Total Sales' AS measure_name, SUM(sales_amount) AS measure_value FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Quantity' AS measure_name, SUM(quantity) AS measure_value FROM gold.fact_sales
+UNION ALL
+SELECT 'Average Price' AS measure_name, AVG(price) AS measure_value FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Nr. Orders' AS measure_name, COUNT(DISTINCT order_number) AS measure_value FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Nr. Products' AS measure_name, COUNT(product_name) AS measure_value FROM gold.dim_products
+UNION ALL
+SELECT 'Total Nr. Customers' AS measure_name, COUNT(customer_key) AS measure_value FROM gold.dim_customers;
